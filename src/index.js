869352aa0ea8474ae940b3b6c56de00f1d2ab6c5
Juan Pablo Olaya <juanpa-1920@hotmail.com>
2021-07-05 19:43:14 -0500

    Adding webpack basic

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { join } from 'lodash';
+
+function component() {
+  const element = document.createElement("div");
+
+  element.innerHTML = join(["Hello", "Webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
